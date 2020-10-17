395b57717a7a7884b0f8d651d3319ee8c4730b62
Sumit Borhade <sumitborhade@gmail.com>
2020-10-18 03:06:40 +0800

    Paint code check-in

--- src/main/java/com/cs/paint/service/validator/impl/LineValidator.java
+++ src/main/java/com/cs/paint/service/validator/impl/LineValidator.java
@@ -13,6 +13,9 @@ import com.cs.paint.utils.GenericUtils;
  */
 public class LineValidator implements ValidatorService {
 
+	/***
+	 * It accepts input and triggers various methods to perform validations.
+	 */
 	@Override
 	public boolean validate(String[] inputArray) {
 		validateIfNumberOfParamsAreCorrect(inputArray);
@@ -21,18 +24,33 @@ public class LineValidator implements ValidatorService {
 		return true;
 	}
 
+	/**
+	 * This method validates if the number of parameters for line are 5 
+	 * and throws exception if this validation is violated.
+	 * @param inputArray
+	 */
 	private void validateIfNumberOfParamsAreCorrect(String[] inputArray) {
 		if (inputArray.length != 5) {
 			throw new CustomException(ApplicationExceptionCode.INCORRECT_LINE_INPUT_PARAMS);
 		}
 	}
 
+	/**
+	 * This method validates if the start and end points's x and y co-ordinates are integer  
+	 * and throws exception if this validation is violated.
+	 * @param inputArray
+	 */
 	private void validateIfThePointsAreInteger(String[] inputArray) {
 		if (!GenericUtils.isInteger(inputArray[1], inputArray[2], inputArray[3], inputArray[4])) {
 			throw new CustomException(ApplicationExceptionCode.LINE_INPUT_SHOULD_BE_INTEGER);
 		}
 	}
 	
+	/**
+	 * This method validates if the start and end points are inside canvas   
+	 * and throws exception if this validation is violated.
+	 * @param inputArray
+	 */
 	private void validateIfTheLinePointsInsideCanvas(String[] inputArray) {
 		PointModel startPoint = new PointModel(Integer.parseInt(inputArray[1]), Integer.parseInt(inputArray[2]));
 		PointModel endPoint = new PointModel(Integer.parseInt(inputArray[3]), Integer.parseInt(inputArray[4]));
